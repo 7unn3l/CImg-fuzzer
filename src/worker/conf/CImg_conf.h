ec9c63dc5095db240d31322ce7f562576a0d23d9
Sgt Supergeil <maxrademacher02@gmail.com>
2022-01-31 22:13:37 +0100

    added worker and Cimg_conf file

---
+++ src/worker/conf/CImg_conf.h
@@ -0,0 +1,2 @@
+// we dont care about displaying capabilities for now
+#define cimg_display 0 
---
+++ src/worker/worker.cpp
@@ -0,0 +1,8 @@
+#include "./conf/CImg_conf.h"
+#include "../CImg/CImg.h"
+
+using namespace cimg_library;
+
+int main(int argc, char* argv[]){
+    return 0;
+}
