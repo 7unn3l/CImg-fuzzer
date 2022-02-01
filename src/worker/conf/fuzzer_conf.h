003cd642692ca4aad4bdab81a24b885a2aaed04d
Sgt Supergeil <maxrademacher02@gmail.com>
2022-02-01 21:57:25 +0100

    changed fuzzable extensions config to avoid redefinition

--- src/worker/conf/fuzzer_conf.h
+++ src/worker/conf/fuzzer_conf.h
@@ -2,14 +2,5 @@
 
 #define fuzzer_corpus_dir "../../sample_corpus" 
 
-const char* FUZZ_EXTENSIONS[8] = {
-    // native support for the following extensions
-    ".raw", // consists in a very simple header (in ascii), then the image data.
-    ".asc", // (Ascii)
-    ".hdr", // (Analyze 7.5)
-    ".inr", // (Inrimage)
-    ".pgm", // (Portable Pixmap)
-    ".bmp", // (uncompressed)
-    ".pan", // (Pandore-5)
-    ".dlm"  // (Matlab ASCII)
-    };
+// edit in fuzzer_conf.cpp
+extern const char* FUZZ_EXTENSIONS[8]; 
