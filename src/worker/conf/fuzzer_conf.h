fd27faea52ea6944d722da52e096addd52cb4775
Sgt Supergeil <maxrademacher02@gmail.com>
2022-02-01 21:35:30 +0100

    added fuzzer configuration on extensions and corpus dir

--- src/worker/conf/fuzzer_conf.h
+++ src/worker/conf/fuzzer_conf.h
@@ -1,1 +1,15 @@
 #define fuzzer_log 1
+
+#define fuzzer_corpus_dir "../../sample_corpus" 
+
+const char* FUZZ_EXTENSIONS[8] = {
+    // native support for the following extensions
+    ".raw", // consists in a very simple header (in ascii), then the image data.
+    ".asc", // (Ascii)
+    ".hdr", // (Analyze 7.5)
+    ".inr", // (Inrimage)
+    ".pgm", // (Portable Pixmap)
+    ".bmp", // (uncompressed)
+    ".pan", // (Pandore-5)
+    ".dlm"  // (Matlab ASCII)
+    };
