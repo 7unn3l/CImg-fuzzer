c149b636589dc650fd556e669941c89850630c48
7unn3l <maxrademacher02@gmail.com>
2022-02-20 22:41:11 +0100

    implemented conditional fileformat loading

--- src/worker/runner/runner.cpp
+++ src/worker/runner/runner.cpp
@@ -3,7 +3,31 @@
 #include "../../CImg/CImg.h"
 
 void Runner::run_one_sample(ImageBytes* sample){
+    cimg_library::CImg<unsigned char> img;
+    
+    try{
+        load_fileformat(sample,img);
+    }catch(cimg_library::CImgIOException){
+
+    }catch(cimg_library::CImgInstanceException){
+        
+    }
+    
+}
+
+void Runner::load_fileformat(ImageBytes* sample, cimg_library::CImg<unsigned char>& img){
+
     FILE* vfile = fmemopen(sample->data,sample->sz,"rw");
 
-    cimg_library::CImg<unsigned char> img;
+    // no case statements with strings? Nice typesystem, c++
+
+    #define EXTENSION(x) sample->fileformat.compare(x) == 0
+
+    if (EXTENSION("bmp")){
+        img._load_bmp(vfile,"");
+    }
+    else if (EXTENSION("ppm") || EXTENSION("pgm")){
+        img._load_pnm(vfile,"");
+    }
+    fclose(vfile);
 }
--- src/worker/runner/runner.h
+++ src/worker/runner/runner.h
@@ -1,8 +1,11 @@
 #pragma once
 #include <cstdio>
 #include "../corpusmanager/corpusmanager.h"
+#include "../conf/CImg_conf.h"
+#include "../../CImg/CImg.h"
 
 class Runner{
 public:
     void run_one_sample(ImageBytes* sample);
+    void load_fileformat(ImageBytes* sample, cimg_library::CImg<unsigned char>& img);
 };
