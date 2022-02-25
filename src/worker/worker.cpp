ab0538364d6ba1862107375200e7ae382a52fc4b
7unn3l <maxrademacher02@gmail.com>
2022-02-25 22:57:23 +0100

    implemented basic main fuzzer loop

--- src/worker/worker.cpp
+++ src/worker/worker.cpp
@@ -1,11 +1,24 @@
-#include "conf/CImg_conf.h"
-#include "conf/fuzzer_conf.h"
-#include "../CImg/CImg.h"
-#include "log/log.h"
+#include "random/random.h"
 #include "corpusmanager/corpusmanager.h"
-
-using namespace cimg_library;
+#include "mutator/mutator.h"
+#include "runner/runner.h"
 
 int main(int argc, char* argv[]){
-    return 0;
+    CorpusManager cm{};
+    Mutator mu{&cm};
+    Runner rn{};
+
+    randomgen::seedrand();
+
+    // avoid printing exception messages to stdout, still allowes
+    // the library to throw them.
+    cimg_library::cimg::exception_mode(0);
+
+    while (true){
+        auto sample = mu.get_mutated_sample();
+        rn.run_one_sample(sample);
+        free(sample->data);
+        delete sample;
+    }
+
 }
