eac2278318714c0e8622e5cc0913fdf9dcb68c70
7unn3l <maxrademacher02@gmail.com>
2022-02-25 23:12:02 +0100

    removed unneeded config options

--- src/worker/conf/fuzzer_conf.h
+++ src/worker/conf/fuzzer_conf.h
@@ -1,6 +1,2 @@
 #define fuzzer_log 1
-
-#define fuzzer_corpus_dir "../../sample_corpus" 
-
-// edit in fuzzer_conf.cpp
-extern const char* FUZZ_EXTENSIONS[8]; 
+#define fuzzer_corpus_dir "../../sample_corpus"
