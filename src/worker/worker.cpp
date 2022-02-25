3eb33aeef1413b53fe7bd50af7585860465e886b
7unn3l <maxrademacher02@gmail.com>
2022-02-25 23:09:21 +0100

    moved setup stuff into setup function

--- src/worker/worker.cpp
+++ src/worker/worker.cpp
@@ -3,16 +3,22 @@
 #include "mutator/mutator.h"
 #include "runner/runner.h"
 
-int main(int argc, char* argv[]){
-    CorpusManager cm{};
-    Mutator mu{&cm};
-    Runner rn{};
 
+void setup(){
     randomgen::seedrand();
 
     // avoid printing exception messages to stdout, still allowes
     // the library to throw them.
     cimg_library::cimg::exception_mode(0);
+
+}
+
+int main(int argc, char* argv[]){
+    CorpusManager cm{};
+    Mutator mu{&cm};
+    Runner rn{};
+
+    setup();
 
     while (true){
         auto sample = mu.get_mutated_sample();
