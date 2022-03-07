2cec2e63b9e49558771b40140e18e4bac69907da
7unn3l <maxrademacher02@gmail.com>
2022-03-07 18:24:23 +0100

    fixed typo

--- src/controller/cli/cli.py
+++ src/controller/cli/cli.py
@@ -6,4 +6,4 @@ parser.add_argument('-num_fuzzers','-n',type=int,metavar='n',help='number of ind
 parser.add_argument('-crash_dir','-c',type=str,metavar='dir',help='directory to store crashes. Deafault ./crashes',default="./crashes")
 parser.add_argument('-update_interval','-i',type=int,metavar='i',help='update interval of ui in seconds. Deafault .125',default=.125)
 parser.add_argument('-corpus_dir','-s',type=str,metavar='dir',help='directory to load corpus from. Deafault ./corpus',default="./corpus")
-parser.add_argument('-binary_path','-b',type=str,metavar='path',help='path to the worker executable ./corpus',default="./bin/worker")
+parser.add_argument('-binary_path','-b',type=str,metavar='path',help='path to the worker executable. Default ./bin/worker',default="./bin/worker")
