69d91882f1d5247209c9e123b44ff514759b418d
7unn3l <maxrademacher02@gmail.com>
2022-03-24 17:57:48 +0100

    fixed exception handeling

--- src/controller/main.py
+++ src/controller/main.py
@@ -1,3 +1,4 @@
+from dis import findlinestarts
 from controller.controller import Controller
 from cli.cli import parser
 import curses
@@ -11,9 +12,14 @@ if __name__ == '__main__':
     # but in this case we want to call c.end() before terminating to ensure
     # that we always clean up shared memory objects on the system.
 
+    exc_inf = None
+
     try:
         curses.wrapper(c.run)
     except:
-        c.end()
         exc_inf = sys.exc_info()
-        raise exc_inf[1].with_traceback(exc_inf[2])
+
+    finally:
+        c.end()
+        if exc_inf:
+            raise exc_inf[1].with_traceback(exc_inf[2])
