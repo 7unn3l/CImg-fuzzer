d2dc9e8fb10f9bff24a92c46ba1bba839585f76f
7unn3l <maxrademacher02@gmail.com>
2022-04-07 15:06:18 +0200

    fixed mem leak error based on unclosed fd

--- src/worker/runner/runner.cpp
+++ src/worker/runner/runner.cpp
@@ -6,14 +6,8 @@
 
 void Runner::run_one_sample(Communicator& com){
     cimg_library::CImg<unsigned char> img;
-    
-    try{
-        load_fileformat(com,img);
-    }catch(cimg_library::CImgIOException){
 
-    }catch(cimg_library::CImgInstanceException){
-        
-    }
+    load_fileformat(com,img);
     
 }
 
@@ -25,40 +19,48 @@ void Runner::load_fileformat(Communicator& com, cimg_library::CImg<unsigned char
 
     #define EXTENSION(x) com.current_extension.compare(x) == 0
     #define LOAD(x) img._load_##x(vfile,"")
+    
+    try{ // many times cimg will correctly detect if an image is invalid and throw an exception
 
-    if (EXTENSION("ascii")){
-        LOAD(ascii);
+        if (EXTENSION("ascii")){
+            LOAD(ascii);
+        }
+        else if (EXTENSION("hdr") || EXTENSION("nii")){
+            img._load_analyze(vfile,"",0);
+        }
+        else if(EXTENSION("inr")){
+            img._load_inr(vfile,"",0);
+        }
+        else if (EXTENSION("pnm") || EXTENSION("ppm") || EXTENSION("pgm")){
+            LOAD(pnm);
+        }
+        else if (EXTENSION("bmp")){
+            LOAD(bmp);
+        }
+        else if (EXTENSION("pan") || EXTENSION("pandore")){
+            LOAD(pandore);
+        }
+        else if (EXTENSION("dlm")){
+            LOAD(dlm);
+        }
+        else if (EXTENSION("jpeg") || EXTENSION("jpg")){
+            LOAD(jpeg);
+        }
+        else if (EXTENSION("png")){
+            img._load_png(vfile,"",0);
+        }
+        else if (EXTENSION("pfm")){
+            LOAD(pfm);
+        }
+        else{
+            LOG("[-] no extension handler defined for extension %s",com.current_extension.c_str());
+            exit(1);
+        }
+
     }
-    else if (EXTENSION("hdr") || EXTENSION("nii")){
-        img._load_analyze(vfile,"",0);
-    }
-    else if(EXTENSION("inr")){
-        img._load_inr(vfile,"",0);
-    }
-    else if (EXTENSION("pnm") || EXTENSION("ppm") || EXTENSION("pgm")){
-        LOAD(pnm);
-    }
-    else if (EXTENSION("bmp")){
-        LOAD(bmp);
-    }
-    else if (EXTENSION("pan") || EXTENSION("pandore")){
-        LOAD(pandore);
-    }
-    else if (EXTENSION("dlm")){
-        LOAD(dlm);
-    }
-    else if (EXTENSION("jpeg") || EXTENSION("jpg")){
-        LOAD(jpeg);
-    }
-    else if (EXTENSION("png")){
-        img._load_png(vfile,"",0);
-    }
-    else if (EXTENSION("pfm")){
-        LOAD(pfm);
-    }
-    else{
-        LOG("[-] no extension handler defined for extension %s",com.current_extension.c_str());
-        exit(1);
-    }
+    catch (cimg_library::CImgIOException) {} 
+    
+    catch (cimg_library::CImgInstanceException) {}
+    
     fclose(vfile);
 }
